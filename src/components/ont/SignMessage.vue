1255719677dbd852082b0225e161ee095cfca833
zeyuyuan <yzy@pcomg.com>
2022-03-31 10:53:42 +0800

    feat: update ontology sign message

--- src/components/ont/SignMessage.vue
+++ src/components/ont/SignMessage.vue
@@ -1,24 +1,44 @@
 <template>
   <n-list-item>
     <n-thing title="message.signMessage">
-      <n-button @click="signMessage">Sign message</n-button>
+      <n-space vertical>
+        <n-input v-model:value="text" placeholder="Message to sign" />
+        <n-button @click="signMessage" :disabled="text.length === 0">
+          Sign message
+        </n-button>
+        <n-button @click="signMessageUseIdentity" :disabled="text.length === 0">
+          Sign message use identity
+        </n-button>
+      </n-space>
     </n-thing>
   </n-list-item>
 </template>
 
 <script setup>
-import { inject } from "vue";
-import { NListItem, NThing, NButton } from "naive-ui";
+import { inject, ref } from "vue";
+import { NListItem, NThing, NButton, NSpace, NInput } from "naive-ui";
 
 const client = inject("connectedClient");
+const text = ref("");
 
 const signMessage = async () => {
   if (!client.value) {
     throw new Error("connect first");
   }
   const result = await client.value.message.signMessage({
-    message: "hello",
+    message: text.value,
     useIdentity: false,
+  });
+  console.log("result", result);
+};
+
+const signMessageUseIdentity = async () => {
+  if (!client.value) {
+    throw new Error("connect first");
+  }
+  const result = await client.value.message.signMessage({
+    message: text.value,
+    useIdentity: true,
   });
   console.log("result", result);
 };
