6752b2da1b6e03ef67fa91f35affda334960d32a
zeyuyuan <yzy@pcomg.com>
2022-03-31 11:30:29 +0800

    feat: update ontology sign message

--- src/components/ont/SignMessage.vue
+++ src/components/ont/SignMessage.vue
@@ -2,12 +2,15 @@
   <n-list-item>
     <n-thing title="message.signMessage">
       <n-space vertical>
-        <n-input v-model:value="text" placeholder="Message to sign" />
+        <n-input v-model:value="text" placeholder="Message to sign/verify" />
+        <n-switch v-model:value="isUseIdentity">Use identity</n-switch>
         <n-button @click="signMessage" :disabled="text.length === 0">
           Sign message
         </n-button>
-        <n-button @click="signMessageUseIdentity" :disabled="text.length === 0">
-          Sign message use identity
+        <n-input v-model:value="signature" placeholder="Signature to verify" />
+        <n-input v-model:value="publicKey" placeholder="Public key to verify" />
+        <n-button @click="verifyMessage" :disabled="!(signature && publicKey)">
+          Verify message
         </n-button>
       </n-space>
     </n-thing>
@@ -16,10 +19,13 @@
 
 <script setup>
 import { inject, ref } from "vue";
-import { NListItem, NThing, NButton, NSpace, NInput } from "naive-ui";
+import { NListItem, NThing, NButton, NSpace, NInput, NSwitch } from "naive-ui";
 
 const client = inject("connectedClient");
 const text = ref("");
+const publicKey = ref("");
+const signature = ref("");
+const isUseIdentity = ref(false);
 
 const signMessage = async () => {
   if (!client.value) {
@@ -27,20 +33,25 @@ const signMessage = async () => {
   }
   const result = await client.value.message.signMessage({
     message: text.value,
-    useIdentity: false,
+    useIdentity: isUseIdentity.value,
   });
-  console.log("result", result);
+  publicKey.value = result.publicKey;
+  signature.value = result.data;
+  console.log("signMessage result:", result);
 };
 
-const signMessageUseIdentity = async () => {
+const verifyMessage = async () => {
   if (!client.value) {
     throw new Error("connect first");
   }
-  const result = await client.value.message.signMessage({
+  const result = await client.value.message.verifyMessage({
     message: text.value,
-    useIdentity: true,
+    signature: {
+      publicKey: publicKey.value,
+      data: signature.value,
+    },
   });
-  console.log("result", result);
+  console.log("verifyMessage result:", result);
 };
 </script>
 
